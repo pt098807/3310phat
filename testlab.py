67994f0f36954f60e8f3ca4b9862df59e350aeb1
pt098807 <664070310050@npu.ac.th>
2025-01-08 23:20:23 +0700

    Update testlab.py

--- testlab.py
+++ testlab.py
@@ -1,2 +1,8 @@
 print ('Hello world!')
 print ('Hello world!')
+print ('Hello world!')
+print ('Hello world!')
+print ('Hello world!')
+print ('Hello world!')
+print ('Hello world!')
+print ('Hello world!')
